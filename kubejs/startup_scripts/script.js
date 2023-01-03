1462144da08de921a302cd14854ea070ef99a05d
Christopher Haws <christopher.haws@arvato-scs.com>
2023-01-02 23:58:00 -0800

    Fix script

--- kubejs/startup_scripts/script.js
+++ kubejs/startup_scripts/script.js
@@ -2,12 +2,12 @@
 
 console.info('Loading server startup script.');
 
-StartupEvents.registry('item', event => {
-	// Register new items here
-	// event.create('example_item').displayName('Example Item')
-})
+// StartupEvents.registry('item', event => {
+// 	// Register new items here
+// 	// event.create('example_item').displayName('Example Item')
+// })
 
-StartupEvents.registry('block', event => {
-	// Register new blocks here
-	// event.create('example_block').material('wood').hardness(1.0).displayName('Example Block')
-})
+// StartupEvents.registry('block', event => {
+// 	// Register new blocks here
+// 	// event.create('example_block').material('wood').hardness(1.0).displayName('Example Block')
+// })
