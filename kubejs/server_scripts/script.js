25d0661f23ef6ed1df740ad4ec96deda5df27ecf
Christopher Haws <christopher.haws@arvato-scs.com>
2023-01-02 23:21:33 -0800

    Fix script error

--- kubejs/server_scripts/script.js
+++ kubejs/server_scripts/script.js
@@ -2,15 +2,15 @@
 
 console.info('Loading server script.');
 
-ServerEvents.tags('item', event => {
-	// https://minecraft.fandom.com/wiki/Redstone_components
-	event.add('crafters:redstone_components', ...[
-		'minecraft:redstone',
-		'minecraft:redstone_torch',
-		'minecraft:lever',
-		'minecraft:repeater',
-		'minecraft:comparator',
-		'minecraft:piston',
-		'minecraft:sticky_piston',
-	]);
-})
+// ServerEvents.tags('item', event => {
+// 	// https://minecraft.fandom.com/wiki/Redstone_components
+// 	event.add('crafters:redstone_components', 
+// 		'minecraft:redstone',
+// 		'minecraft:redstone_torch',
+// 		'minecraft:lever',
+// 		'minecraft:repeater',
+// 		'minecraft:comparator',
+// 		'minecraft:piston',
+// 		'minecraft:sticky_piston',
+// 	);
+// })
